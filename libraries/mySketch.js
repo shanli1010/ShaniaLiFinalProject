e5c1d5ad509559e938312a126815ee2ed51e44f1
shanli1010 <90589845+shanli1010@users.noreply.github.com>
2021-12-12 17:46:59 -0500

    Push #4
    Figured out shooting function for Tom character selection

--- libraries/mySketch.js
+++ libraries/mySketch.js
@@ -9,6 +9,20 @@ let bg;
 var s;
 var score = 0;
 
+let j;
+
+/////
+
+var i=0;
+var x1=710;
+var y1=700;
+var go=0;
+var arr=[];
+var shooting;
+var bulletx=1000;
+var bullety=1000;
+  //
+
 function preload() {
   TJ = loadImage("tomBG.jpg"); //All Background Images TV Show settings 
   PPG = loadImage("PPG.jpeg");
@@ -22,18 +36,14 @@ function preload() {
   xirod = loadFont("xirod.ttf"); //Title Font
   start = loadImage("startbutton.png");
 
-  jerry = createSprite(305, 300, 200, 200);
-  jerry.addImage(loadImage("jerry2.png"));
+//   jerry = createSprite(305, 300, 100, 100);
+//   //jerry.addImage(loadImage("jerry2.png"));
 
-  jerry2 = createSprite(1005, 300, 200, 200);
-  jerry2.addImage(loadImage("jerry2.png"));
+//   jerry2 = createSprite(1005, 300);
+//   jerry2.addImage(loadImage("jerry2.png"));
 
-  jerry3 = createSprite(1605, 300, 200, 200);
-  jerry3.addImage(loadImage("jerry2.png"));
-
-
-
-
+//   jerry3 = createSprite(1605, 300);
+//   jerry3.addImage(loadImage("jerry2.png"));
 }
 
 
@@ -69,7 +79,7 @@ function draw() {
     image(tom, 105, 500, 500, 500);
 
     fill(119,213,241);
-    rect(710, 500, 500,500); //Bubble Character Selection
+    rect(710, 500, 500,500); //Bubbles Character Selection
     image(bubbles, 710, 500, 500, 500);
 
     fill(127,193,67);
@@ -81,8 +91,14 @@ function draw() {
   {
     scene2 = false; 
     background(TJ);
-    image(tom,710,700,500,500);
+    // image(tom,710,700,500,500);
     drawSprites();
+    // sprite.shoot();
+     gun();
+    shotdisplay(); 
+     
+   enemy();
+  
   }
   else if (scene4 == true) //Bubbles Screen
   {
@@ -96,40 +112,9 @@ function draw() {
 
 }
 
+
+
 function mousePressed(){
-
-
-
-  // switch(sceneCounter)
-  // {
-  //   case 0:
-  //     break;
-
-  //   case 1:
-  //     if (scene1 == true)
-  //       {
-  //        if (((mouseX > 630) && (mouseX < 1230)) && ((mouseY > 500) && (mouseY < 750)))  //Start Button
-  //          {
-  //             scene2 = true;
-  //           }   
-  //           scene1 = false;
-  //           sceneCounter++;
-  //         }
-  //         break;
-  //   case 2:
-  //     if (scene2 == true)
-  //     {
-  //       if (((mouseX > 105)&&(mouseX > 605)) && ((mouseY > 500) && (mouseY < 1000 )))  //Tom Button
-  //          {
-  //               scene3 = true;
-  //          }
-  //       scene2 = false;
-  //       sceneCounter++; 
-  //     }
-  // }
-
-
-
   if (scene1 == true)
   {
    if (((mouseX > 630) && (mouseX < 1230)) && ((mouseY > 500) && (mouseY < 750)))  //Start Button
@@ -139,11 +124,6 @@ function mousePressed(){
    if (((mouseX > 105)&&(mouseX < 605)) && ((mouseY > 500) && (mouseY < 1000 )))  //Tom Button
    {
         scene3 = true;
-        
-        s = createSprite(mouseX, mouseY, 30, 30);
-        s.velocity.x = random(-5, 0);
-        s.velocity.y = random(-5, 0);
-        
    }
   //  // // else if ((mouseX<  )&&(mouseX>)&& (mouseY<  )&&(mouseY>  ))  Bubble Button
   //  // {
@@ -164,6 +144,115 @@ function mousePressed(){
 //     rect(105,900,200,100);
 //   }
 
+
+}
+
+// class balloon{
+  
+//   constructor(x,y){
+//     this.xpos = mouseX;
+//     this.ypot = mouseY;
+//      this.sprite = createSprite(1000,1000);
+//      sprite.addImage(loadImage("waterBalloon.png"));
+  
+// }
+
+
+//   shoot(){
+
+//   bullet.velocity.x = random(-5, 0);
+//   bullet.velocity.y = random(-5, 0);
+
+// }
+// }
+//////////////////////
+function bubble(x){
+
+  this.x=x;
+  this.y=40;
+  this.display=function (){
+    if(dist(this.x,this.y,bulletx,bullety)<30){
+       rem(i);
+       }
+    else{
+  
+   fill(255, 0, 0);
+   noStroke();
+    ellipse(this.x,this.y,50,50);
+
+     jerry3 = createSprite(this.x, this.y, 100, 100);
+     jerry3.addImage(loadImage("jerry2.png"));
+
+    }
+  }
+}
+
+
+function add(){
+   let x=random(100,1800);
+   let bubbles=new bubble(x);
+  arr.push(bubbles);
+}
+
+function gun(){
+ 
+    if (keyIsDown(LEFT_ARROW)) {
+    
+      
+      if(x1<-160){
+      
+      }
+      else{
+        x1-=10;
+      }
+      
+    }
+  
+    if (keyIsDown(RIGHT_ARROW)) {
+        if(x1>1700){
+        
+      }
+      else{
+        x1+=10;
+      }
+    }
+
+ image(tom,x1,y1,500,500);
+}
+
+
+
+function shotdisplay(){
+  if(bullety<0){
+    
+    bullety=900;
+  }
+  else{
+    bullety-=5;
+  }
+  bulletx=x1+200;
+   strokeWeight(5);
+   stroke(255);
+   fill(88,153,209);
+  ellipse(bulletx,bullety,60);
+  //j = createSprite(bulletx,bullety, 10,10);
+
+}
+
+
+
+function rem(index){
+  arr.splice(i,1);
+}
+
+function enemy(){
+
+for(i=0;i< arr.length;i++){
+   arr[i].display();
+ }  
+  if(arr.length==0){
+    add();
+  }
 
 }
 
